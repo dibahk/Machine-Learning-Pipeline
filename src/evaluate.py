ebab302de4cc5620a286c64b6787166bbff1b48e
dibahk <dibahadjikazemi@gmail.com>
2025-01-12 20:39:14 +0000

    adding evaluation

---
+++ src/evaluate.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+import yaml
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from mlflow.models import infer_signature
+import os
+from sklearn.model_selection import train_test_split, GridSearchCV
+from urllib.parse import urlparse
+import mlflow
+
+os.environ["MLFLOW_TRACKING_URI"] = "https://dagshub.com/dibahk/Machine-Learning-Pipeline.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"] = "dibahk"
+os.environ["MLFLOW_TRACKING_PASSWORD"] = "4f087248fdd4b1542c5af6ee0c98b3e76a9cc372"
+
+params = yaml.safe_load(open("params.yaml"))["train"]
+
+def evaluate(data_path, model_path):
+    data = pd.read_csv(data_path)
+    x = data.drop(columns= ["Outcome"])
+    y = data["Outcome"]
+
+    mlflow.set_tracking_uri("https://dagshub.com/dibahk/Machine-Learning-Pipeline.mlflow")
+
+    model = pickle.load(open(model_path, "rb"))
+
+    predictions = model.predict(x)
+    accuracy = accuracy_score(y, predictions)
+
+    mlflow.log_etric("accuract", accuracy)
+    print(f"Model accuracy: {accuracy}")
+
+if __name__ == "__main__":
+    evaluate(data_path= params["data"], model_path= params["model"])
---
+++ src/train.py
@@ -0,0 +1,74 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+import yaml
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from mlflow.models import infer_signature
+import os
+from sklearn.model_selection import train_test_split, GridSearchCV
+from urllib.parse import urlparse
+import mlflow
+
+os.environ["MLFLOW_TRACKING_URI"] = "https://dagshub.com/dibahk/Machine-Learning-Pipeline.mlflow"
+os.environ["MLFLOW_TRACKING_USERNAME"] = "dibahk"
+os.environ["MLFLOW_TRACKING_PASSWORD"] = "4f087248fdd4b1542c5af6ee0c98b3e76a9cc372"
+
+def hyperparameter_tuning(x_train, y_train, param_grid):
+    rf = RandomForestClassifier()
+    grid_search = GridSearchCV(estimator=rf, param_grid= param_grid, cv= 3, n_jobs= -1, verbose= 2)
+    grid_search.fit(x_train, y_train)
+    return grid_search
+
+# load parameters from params.yaml
+params = yaml.safe_load(open("params.yaml"))["train"]
+
+def train(data_path, model_path, random_state, n_estimators):
+    data = pd.read_csv(data_path)
+    x = data.drop(columns= ["Outcome"])
+    y = data["Outcome"]
+
+    mlflow.set_tracking_uri("https://dagshub.com/dibahk/Machine-Learning-Pipeline.mlflow")
+
+    with mlflow.start_run():
+        # split the dataset into training and test sets
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size= 0.2)
+        signature = infer_signature(x_train, y_train)
+
+        param_grid= {
+            'n_estimators': [100, 200],
+            'max_depth': [5, 10, None],
+            'min_samples_split': [2, 5],
+            'min_samples_leaf': [1, 2]
+        }
+        grid_search = hyperparameter_tuning(x_train, y_train, param_grid)
+
+        best_model = grid_search.best_estimator_
+        y_pred = best_model.predict(x_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        print(f"Accuracy: {accuracy}")
+        mlflow.log_metric("accuracy", accuracy)
+        mlflow.log_param("n_estimators", grid_search.best_params_["n_estimators"])
+        mlflow.log_param("max_depth", grid_search.best_params_["max_depth"])
+        mlflow.log_param("min_sample_split", grid_search.best_params_['min_samples_split'])
+        mlflow.log_param("min_sample_leaf", grid_search.best_params_['min_samples_leaf'])
+
+        cm = confusion_matrix(y_test, y_pred)
+        cr = classification_report(y_test, y_pred)
+
+        mlflow.log_text(str(cm), "confusion_matrix.txt")
+        mlflow.log_text(cr, "classification_report.txt")
+
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        if tracking_url_type_store != "file":
+            mlflow.sklearn.log_model(best_model, "model", registered_model_name= "Best Model")
+        else:
+            mlflow.sklearn.log_model(best_model, "model", signature= signature)
+
+        os.makedirs(os.path.dirname(model_path), exist_ok= True) 
+        filename= model_path
+        pickle.dump(best_model, open(filename, "wb"))
+        print(f"model saved to {model_path}")
+
+if __name__ == "__main__":
+    train(params["data"], params["model"], params["random_state"], params["n_estimators"])
